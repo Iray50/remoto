4810e0ff32c580292c0499ec0702f247019973de
iratxe <icr7808@gmail.com>
2021-03-16 18:09:43 +0100

    Creado ArrayList para meter los datos

--- Ejercicio1-2-3/src/Main.java
+++ Ejercicio1-2-3/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -5,6 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		String nom="",nom1="",nom2="",nom3="",nom4="",nom5="";
 		int opc=0;
+		ArrayList<String> estudiantes = new ArrayList<String>();
 		Scanner sc = new Scanner(System.in);
 		Scanner sn = new Scanner(System.in);
 
@@ -23,6 +25,14 @@ public class Main {
 			nom4=sc.nextLine();
 			System.out.println("Introduzca su teléfono: ");
 			nom5=sc.nextLine();
+			
+			estudiantes.add(nom);
+			estudiantes.add(nom1);
+			estudiantes.add(nom2);
+			estudiantes.add(nom3);
+			estudiantes.add(nom4);
+			estudiantes.add(nom5);
+			
 		}
 	}
 }
