39ac2119322f3208026051e6f1f69952f9394712
iratxe <icr7808@gmail.com>
2021-03-16 17:44:44 +0100

    Lineas para pedir los datos al usuario

---
+++ Ejercicio1-2-3/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nom="",nom1="",nom2="",nom3="",nom4="",nom5="";
+		int opc=0;
+		Scanner sc = new Scanner(System.in);
+
+			System.out.println("Introduzca su nombre: ");
+			nom=sc.nextLine();
+			System.out.println("Introduzca su apellido: ");
+			nom1=sc.nextLine();
+			System.out.println("Introduzca su fecha de nacimiento: ");
+			nom2=sc.nextLine();
+			System.out.println("Introduzca su DNI: ");
+			nom3=sc.nextLine();
+			System.out.println("Introduzca su estudios previos: ");
+			nom4=sc.nextLine();
+			System.out.println("Introduzca su teléfono: ");
+			nom5=sc.nextLine();
+	}
+}
